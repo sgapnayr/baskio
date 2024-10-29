5726511a1e718b75107e5637888e964a395ee571
sgapnayr <100978214+sgapnayr@users.noreply.github.com>
2024-10-28 19:11:10 -0500

    Init

---
+++ Landing/README.md
@@ -0,0 +1,3 @@
+# Baskio-React
+
+Baskio-React
---
+++ Landing/src/commonsections/HeadTitle.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import React from "react";
+
+const HeadTitle = ({ title }: any) => {
+  return (
+    <React.Fragment>
+      <Head>
+        <title>
+          {title} | Baskio - Clean, Versatile, Responsive Bootstrap 5 & Nextjs
+          Theme
+        </title>
+      </Head>
+    </React.Fragment>
+  );
+};
+export default HeadTitle;
